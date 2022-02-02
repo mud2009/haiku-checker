c65fca39cb4a731548009675c318859014f31a55
Will Greenberg <greenberg.will@gmail.com>
2022-02-02 11:39:52 -0800

    update package.json webpack.config.js
    Co-authored-by: Will Watkins <wjwat@onionslice.org>
    Co-authored-by: jwtill <jwtill@users.noreply.github.com>

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: 'Shape Tracker',
+      title: 'Haiku Chcker',
       template: './src/index.html',
       inject: 'body'
     })
