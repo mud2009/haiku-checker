1a7f11d70ac00a56ae9061dbc678175a0675eaa6
Will Greenberg <greenberg.will@gmail.com>
2022-02-02 14:06:01 -0800

    pass Haiku constructor test
    Co-authored-by: Will Watkins <wjwat@onionslice.org>
    Co-authored-by: jwtill <jwtill@users.noreply.github.com>

---
+++ src/haiku.js
@@ -0,0 +1,23 @@
+//import {syllable} from 'syllable';
+
+export default class Haiku {
+  constructor(text) {
+    this.text = text;
+  }
+}
+// 'An ocean voyage. As waves break over the bow. The sea welcomes me.' -> split into sections -> check each section -> if all sections are true then it's a haiku
+
+// 'An ocean voyage. As waves break over the bow. The sea welcomes me.' -> pre-split -> pass each as parameter -> check each parameter -> if each is # of syllables then it's a haiku
+
+// let x = 'An ocean voyage. As waves break over the bow. The sea welcomes me.';
+
+// console.log(syllable(x));
+
+// let h = new Haiku(string from user);
+
+// 'An ocean voyage.\nAs waves break over the bow.\nThe sea welcomes me.'
+
+// "An ocean voyage.
+// As waves break...
+// "
+// \n
