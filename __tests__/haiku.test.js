b5e9f39464fe159c4a85d626e4a7aa02cce62c4a
Will Greenberg <greenberg.will@gmail.com>
2022-02-02 13:55:26 -0800

    add test for Haiku constructor
    Co-authored-by: Will Watkins <wjwat@onionslice.org>
    Co-authored-by: jwtill <jwtill@users.noreply.github.com>

---
+++ __tests__/haiku.test.js
@@ -0,0 +1,9 @@
+import Haiku from "../src/haiku.js"
+
+describe ('Haiku', () => {
+
+  test('should construct Haiku object', () => {
+    let haiku = new Haiku("text");
+    expect(haiku.text).toEqual("text");
+  });
+});
