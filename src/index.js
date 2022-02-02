341fe8c0725f28f124baacc299e69bb6ae5f24d0
Will Greenberg <greenberg.will@gmail.com>
2022-02-02 11:59:45 -0800

    add boilerplate for index.html and index.js
    Co-authored-by: Will Watkins <wjwat@onionslice.org>
    Co-authored-by: jwtill <jwtill@users.noreply.github.com>

---
+++ src/index.js
@@ -0,0 +1,7 @@
+// I had plans. But sure, ok then. I guess not.
+// An ocean voyage. As waves break over the bow. The sea welcomes me.
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/styles.css';
+
