2bca33ab56e046eed29235bdc67398737914edbf
Will Greenberg <greenberg.will@gmail.com>
2022-02-03 10:02:40 -0800

    add third test for isHaiku, pass all tests
    Co-authored-by: Will Watkins <wjwat@onionslice.org>
    Co-authored-by: jwtill <jwtill@users.noreply.github.com>

--- __tests__/haiku.test.js
+++ __tests__/haiku.test.js
@@ -19,16 +19,18 @@ describe ('Haiku.isThreeLines', () => {
 });
 
 describe ('Haiku.isHaiku', () => {
-  let haiku;
-
-  beforeEach(() => {
-    haiku = new Haiku("An ocean voyage\nAs waves break over the bow\nThe sea welcomes me.")
-  });
-
   test('it should return true if the text is a haiku', () => {
+    let haiku = new Haiku("An ocean voyage\nAs waves break over the bow\nThe sea welcomes me.")
     expect(haiku.isHaiku()).toEqual(true);
+  });
+  test('it should return false if the text is not a haiku', () => {
+    let haiku = new Haiku("An ocean voyage\nAs waves break over the bow\nThe sea me.\nhi ryan!");
+    expect(haiku.isHaiku()).toEqual(false);
+  });
+  test('it should return false if the text is not a haiku', () => {
+    let haiku = new Haiku("An ocean voyage\nAs waves break over the bow\nThe sea welcomes!");
+    expect(haiku.isHaiku()).toEqual(false);
   })
-  
 });
 /*
 describe('Triangle', () => {
--- src/haiku.js
+++ src/haiku.js
@@ -1,4 +1,4 @@
-// const syllable = require('syllable');
+import syllables from 'syllable';
 
 export default class Haiku {
   constructor(text) {
@@ -14,23 +14,19 @@ export default class Haiku {
   }
 
   isHaiku() {
-    let lineArray = [];
-    let sylCounter = 0;
-    let vowels = ["a", "e", "i", "o", "u"];
-    lineArray = this.text.split("\n");
-    for (let i = 0; i<lineArray.length; i++) {
-      let wordArray = lineArray[i].split(" ");
-      for (let j = 0; j < wordArray.length; j++){
-        let letterArray = wordArray[j].split("");
-        for (let k = 0; k < letterArray.length; k++){
-          if (vowels.includes(letterArray[i])) {
-            sylCounter ++;
-          }
-        }
+    if (this.isThreeLines()) {
+      const [lineOne, lineTwo, lineThree] = this.text.split('\n');
+
+      if ((syllables(lineOne) === 5) &&
+          (syllables(lineTwo) === 7) &&
+          (syllables(lineThree) === 5)) {
+        return true;
+      } else {
+        return false;
       }
+    } else {
+      return false;
     }
-    console.log("Counter:", sylCounter);
-    return true;
   }
 }
 
